ac7b906f7a1144b912d948daa7163351a27ac4f1
Francisco <franciscovargasmarcal@gmail.com>
2025-04-11 21:39:24 -0300

    estruturando a Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,38 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <h1>home</h1>
+    <body class="body-home">
+      <main>
+        <div class="banner-home"> {/*banner de fundo e o h1, p e button por cima dele*/}
+          <img src='#' alt='banner-melodia'/>
+          <h1>Melodia</h1>
+          <p>Sua Música, Sua Forma</p>
+          <button class="#">Ouvir Agora</button>
+        </div>
+
+        {/*TAILWIND/sessão apresentação:
+        uma sessão com 3 cards falando sobre os benefícios do App
+        obs: devemos utilizar icones + tailwind aqui*/}
+
+
+        {/*TAILWIND/sessão funcionalidades*/}
+
+
+        {/*TAILWIND/sessão planos:
+        colocar um container, e 5 cards para 
+        mostrar os planos disponíveis:
+        Melô Individual, duo, família, universitário e gratuito*/}
+
+
+        {/*TAILWIND/sessão depoimentos:
+        podemos colocar cards em algum tipo de carrossel manual
+        onde voce clica e o depoimento passa para o prox*/}
+
+        
+        {/*TAILWIND/sessão contato:
+        fazer um formulário para coletar emails via Tailwind*/}
+      </main>
+    </body>
     
     </>
   )
