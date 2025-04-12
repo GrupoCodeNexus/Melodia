f1a684dfada53e528f8b74dbf808d3bcbffbb782
Francisco <franciscovargasmarcal@gmail.com>
2025-04-12 13:22:29 -0300

    posicionando elementos do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/style.css'
+import logo from '../assets/logo.png'
 
 const Nav = () => {
   return (
     <>
-    <section className="c-nav"> {/*container nav*/}
-      <img src='#' alt='logo-melodia'/>
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="#" className="nav-link">Planos</Link>
-      <Link to="#" className="nav-link">Depoimentos</Link>
-      <Link to="#" className="nav-link">Contato</Link>
-    </section>
+
     </>
   )
 }
