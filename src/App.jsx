a0cc52ed8c25818ea3ec5c7d8c39eebb0f07912d
Francisco <franciscovargasmarcal@gmail.com>
2025-04-11 21:02:40 -0300

    limpando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Error from './routes/Error'
+import Home from './routes/Home'
 
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="*" element={<Error/>}/>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
     </>
   )
 }
