bb64401d86ddea48b6dfb3f1fb550b976d5021d9
Francisco <franciscovargasmarcal@gmail.com>
2025-04-14 13:28:14 -0300

    criando cards para section planos e estilizando

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -18,20 +20,20 @@ const Home = () => {
 
       {/* Conteúdo sobre a imagem */}
           <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-white text-center px-4">
-            <nav className="Nav absolute top-0 left-0 w-full flex items-center justify-between p-4 text-white">
+            <nav className="Nav absolute top-0 left-0 w-full flex items-center justify-between text-white">
               <img src='../src/assets/logoMelo.png'
               alt='logo-melodia'/>
               <ul className="flex gap-6 items-center">
                 <li><a href='#' className='transition ease-out duration-700 hover:text-green-500'>Benefícios</a></li>
                 <li><a href='#' className='transition ease-out duration-700 hover:text-green-500'>Depoimentos</a></li>
                 <li><a href='#' className='transition ease-out duration-700 hover:text-green-500'>Contato</a></li>
-                <li><a href='#' className='btn-nav inline-block bg-gradient-to-r from-green-800 to-lime-500 rounded px-5 py-2 transition transform duration-300 hover:scale-105 drop-shadow-lg'>Planos</a></li>
+                <li><a href='#' className='btn-nav inline-block bg-gradient-to-r p-4 from-green-800 to-lime-500 rounded px-5 py-2 transition transform duration-300 hover:scale-105 drop-shadow-lg'>Planos</a></li>
               </ul>
             </nav>
 
           <h1 className="text-5xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-lime-500 drop-shadow-lg animate-pulse">Melodia</h1>
           <p className="text-xl mb-6">Sua Música, Sua Forma</p>
-          <button className="btn-perso bg-green-600 bg-gradient-to-r from-green-900 to-lime-500 drop-shadow-lg hover:cursor-pointer text-white rounded">
+          <button className="m-[4rem]">
             Ouvir Agora
           </button>
         </div>
@@ -46,10 +48,32 @@ const Home = () => {
         {/*TAILWIND/sessão funcionalidades*/}
 
 
-        {/*TAILWIND/sessão planos:
-        colocar um container, e 5 cards para 
-        mostrar os planos disponíveis:
-        Melô Individual, duo, família, universitário e gratuito*/}
+        {/*sessão planos:*/}
+        <div className='pad20 flex items-center bg-gray-800 justify-center gap-20'>
+
+          <div className='pad20 text-white border-2 border-green-500 rounded text-center transition-transform duration-500 ease hover:scale-105'>
+            <FontAwesomeIcon icon={faUser} size='lg'/>
+            <h3 className='padm5'>Melô Individual</h3>
+            <p>Desfrute de acesso ilimitado a todo o catálogo musical do Melô no seu próprio ritmo. Crie playlists personalizadas, ouça offline e descubra novos artistas e músicas. Uma experiência musical completa para um único usuário.</p>
+            <button className='btn-perso depoimento bg-green-500 rounded hover:bg-green-600'>Assinar</button>
+          </div>
+
+          <div className='pad20 text-white border-4 border-green-500 rounded text-center transition-transform duration-500 ease hover:scale-105'>
+            <FontAwesomeIcon icon={faUser} size='lg'/>
+            <FontAwesomeIcon icon={faUser} size='lg'/>
+            <h3 className='padm5'>Melô Duo</h3>
+            <p>Perfeito para casais, amigos ou familiares que moram juntos. Dois perfis individuais com todos os benefícios do Melô Individual, permitindo que cada um explore suas preferências musicais sem interferir no outro. Uma forma econômica de compartilhar a paixão pela música.</p>
+            <button className='btn-perso depoimento bg-green-500 rounded hover:bg-green-600'>Assinar</button>
+          </div>
+
+          <div className='pad20 text-white border-2 border-green-500 rounded text-center transition-transform duration-500 ease hover:scale-105'>
+            <FontAwesomeIcon icon={faUsers} size='lg'/>
+            <h3 className='padm5'>Melô Família</h3>
+            <p>Reúna toda a família com até seis contas individuais sob uma única assinatura. Cada membro terá seu próprio perfil, playlists e recomendações personalizadas. Uma solução acessível para que todos em casa aproveitem milhões de músicas.</p>
+            <button className='btn-perso depoimento bg-green-500 rounded hover:bg-green-600'>Assinar</button>
+          </div>
+        </div>
+
 
 
         {/*TAILWIND/sessão depoimentos:
